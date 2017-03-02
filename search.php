3cb153f00f8ac08d3bdf0890df3a842d7043a4ba
Hyeyeon Yang <imyeonn@gmail.com>
2017-03-02 17:33:57 +0900

    commit test

---
+++ search.php
@@ -0,0 +1,126 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>카테고리</title>
+		<link rel="stylesheet" href="search.css">
+	</head>
+	<body>
+		<div>
+			<div>
+				<img src="images/home/u52.png" />
+				<div class="search">
+		            <!-- Ex 3: Modify forms -->
+		            <form class="search-form" action="index.html" method="get">
+		                <input type="text" name="query" placeholder="제품명검색">
+		                <input type="submit" value="search">
+		            </form>
+		        </div>
+			</div>
+
+			<div style = "clear : both">
+				<div >
+					<p>카테고리</p>
+				</div>
+				<div >
+					<p>플레이리스트</p>
+				</div>
+
+			</div>
+			<div style = "clear : both">
+
+			</div>
+			<article>
+				<div>
+	                <div>
+	                    <img src ="fa fa-arrow-right" alt = "product image"/>
+	                </div>
+	                <div>
+	                	<p>
+	                    	<span>제품명1</span>
+	                    </p>
+	                    <p>
+	                    	<span>가격</span>
+	                    	<span>브랜드명</span>
+	                    </p>
+	                    <p>
+	                    	<span>등장횟수</span>
+	                    	<span>등장영상</span>
+	                    </p>
+	                </div>
+	                <div>
+	                    <p>
+	                    	<span>영상링크1</span>
+	                    </p>
+	                    <p>
+	                    	<span>영상링크2</span>
+	                    </p>
+	                    <p>
+	                    	<span>영상링크3</span>
+	                    </p>
+	                </div>                
+	            </div>
+
+	            <div>
+	                <div>
+	                    <img src ="fa fa-arrow-right" alt = "product image"/>
+	                </div>
+	                <div>
+	                	<p>
+	                    	<span>제품명2</span>
+	                    </p>
+	                    <p>
+	                    	<span>가격</span>
+	                    	<span>브랜드명</span>
+	                    </p>
+	                    <p>
+	                    	<span>등장횟수</span>
+	                    	<span>등장영상</span>
+	                    </p>
+	                </div>
+	                <div>
+	                    <p>
+	                    	<span>영상링크1</span>
+	                    </p>
+	                    <p>
+	                    	<span>영상링크2</span>
+	                    </p>
+	                    <p>
+	                    	<span>영상링크3</span>
+	                    </p>
+	                </div>                
+	            </div>
+	            <div>
+	                <div>
+	                    <img src ="fa fa-arrow-right" alt = "product image"/>
+	                </div>
+	                <div>
+	                	<p>
+	                    	<span>제품명3</span>
+	                    </p>
+	                    <p>
+	                    	<span>가격</span>
+	                    	<span>브랜드명</span>
+	                    </p>
+	                    <p>
+	                    	<span>등장횟수</span>
+	                    	<span>등장영상</span>
+	                    </p>
+	                </div>
+	                <div>
+	                    <p>
+	                    	<span>영상링크1</span>
+	                    </p>
+	                    <p>
+	                    	<span>영상링크2</span>
+	                    </p>
+	                    <p>
+	                    	<span>영상링크3</span>
+	                    </p>
+	                </div>                
+	            </div>
+			</article>
+		</div>
+
+	</body>
+</html>
