cba199e5d3ee91d2a7444c3451344b29c35111fc
Hyeyeon Yang <imyeonn@gmail.com>
2017-03-16 10:05:45 +0900

    img 1:1

--- html/search.php
+++ html/search.php
@@ -81,7 +81,15 @@
 				          $count = $counts[0];
      			?>
 					<div class="productbox">
-						<a href=<?=$productlink?>><span class="imagebox"><img src=<?=$productimage?> onERROR="this.src='images/alt_productimage.png'" /></span></a>
+						<a href=<?=$productlink?>><span class="imagebox">
+							<span class="imgbox">
+								<img src=<?=$productimage?> onERROR="this.src='images/alt_productimage.png'"/>
+							</span>
+						</span></a>
+
+
+
+
 						<div class="namename">
 							<a href=<?=$productlink?>><h2 -ms-overflow-style: none;><?=$productname?></h2></a>
 	            <ul>
