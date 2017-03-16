f3419019a2cfba1c1d0cf76892b28ba046eaa706
Hyeyeon Yang <imyeonn@gmail.com>
2017-03-16 18:31:06 +0900

    analytics

--- html/category.php
+++ html/category.php
@@ -1,6 +1,17 @@
 <!DOCTYPE html>
 <html>
 	<head>
+		<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-90403003-2', 'auto');
+  ga('send', 'pageview');
+
+		</script>
+
 		<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
 		<title>카테고리</title>
 		<meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, user-scalable=no"/>
@@ -199,6 +210,8 @@
 	       	}
 	      ?>
 			</article>
+
+			<div class="blank"></div>
 
 			<div id="forline"></div>
 
